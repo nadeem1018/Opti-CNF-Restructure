d519eefbfada91fb70b4dae7a1620b448f3bd75e
satyarth2 <satyarth2@gmail.com>
2021-01-15 19:08:29 +0530

    add button for custome view in feature bom screen

--- OptiPro-Configurator/src/app/app.component.ts
+++ OptiPro-Configurator/src/app/app.component.ts
@@ -13,7 +13,7 @@ export class AppComponent implements OnInit {
   compactLayout = false;
 
   public svgFile:string = 'assets/images/svg/svg-sprite.svg';
-  public svgVersion:any = "1.02";
+  public svgVersion:any = "1.03";
 
   constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
 
