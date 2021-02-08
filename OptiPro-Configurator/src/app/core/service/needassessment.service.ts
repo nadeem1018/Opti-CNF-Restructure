9b896aef0508e0c2130e8f021cca4a37545f158d
satyarth2 <satyarth2@gmail.com>
2021-02-08 13:07:46 +0530

    fix assessment  save api issue

--- OptiPro-Configurator/src/app/core/service/needassessment.service.ts
+++ OptiPro-Configurator/src/app/core/service/needassessment.service.ts
@@ -27,9 +27,11 @@ export class NeedassessmentService {
 
     AddUpdateNeedAssessmentData(final_dataset_to_save): Observable<any> {
       let cache_control = this.common_params.random_string(40);
-      var jObject = { GetData: JSON.stringify(final_dataset_to_save) };
       final_dataset_to_save.OPCONFIG_NeedsAssessment[0]['GUID'] = sessionStorage.getItem("GUID");
-      final_dataset_to_save.OPCONFIG_NeedsAssessment[0]['UsernameForLic'] = sessionStorage.getItem("loggedInUser")
+      final_dataset_to_save.OPCONFIG_NeedsAssessment[0]['UsernameForLic'] = sessionStorage.getItem("loggedInUser");
+      final_dataset_to_save.OPCONFIG_NeedsAssessment[0]['CompanyDBID'] = sessionStorage.selectedComp;      
+      var jObject = { GetData: JSON.stringify(final_dataset_to_save) };
+     
       //Return the response form the API  
       return this.httpclient.post(this.config_params.service_url + "/NeedsAssessment/AddUpdateNeedsAssessment?cache_control=" + cache_control, jObject, this.common_params.httpOptions);
     }
