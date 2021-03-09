7f8f1ce8a376cb7b0469fa2385d7db211acb2549
Sanyam Jain <sjain@optiproerp.com>
2021-03-09 15:25:42 +0530

    code changes for menu

--- OptiPro-Configurator/src/app/@layout/sidebar/sidebar.component.ts
+++ OptiPro-Configurator/src/app/@layout/sidebar/sidebar.component.ts
@@ -11,6 +11,8 @@ import { CommonService } from 'src/app/core/service/common.service';
 export class SidebarComponent implements OnInit {
   navList: Array<Object> = [];
   navNeedAssesmentList: Array<Object> = [];
+  needIsToggled = false;
+
 
   constructor(
     private router: Router,
@@ -18,6 +20,7 @@ export class SidebarComponent implements OnInit {
   ) { }
   public commonData = new CommonData();
   public language = JSON.parse(sessionStorage.getItem('current_lang'));
+  needMenuData = [{ "itemCode": "208", "itemName": "Need's Assesment", "itemNav": "/need-assessment", "itemIcon": "#assessmentScreen", "itemIconSize": "0 0 512 512", "permission": true }];
   ngOnInit() {
     let temp_menu_data = [
       {
@@ -28,6 +31,7 @@ export class SidebarComponent implements OnInit {
       },
       { "itemCode": "211", "itemName": this.language.config_need_assesment, "itemNav": "/configuration-needAssesment", "itemIcon": "#configurationAssesment", "itemIconSize": "0 0 512 512", "permission": true },
       { "itemCode": "200", "itemName": this.language.itemcodegeneration, "itemNav": "/item-code-generation", "itemIcon": "#code", "itemIconSize": "0 0 612 619.2", "permission": true },
+      { "itemCode": "205", "itemName": this.language.attribute, "itemNav": "/attribute", "itemIcon": "#attributeMaster", "itemIconSize": "0 0 512 512", "permission": true },
       { "itemCode": "201", "itemName": this.language.model_feature_master, "itemNav": "/feature", "itemIcon": "#featureModal", "itemIconSize": "0 0 512 512", "permission": true },
       { "itemCode": "202", "itemName": this.language.feature_Bom, "itemNav": "/feature-bom", "itemIcon": "#featureBom", "itemIconSize": "0 0 395.636 395.636", "permission": true },
       { "itemCode": "203", "itemName": this.language.Model_Bom, "itemNav": "/model-bom", "itemIcon": "#modelBom", "itemIconSize": "0 0 512 512", "permission": true },
@@ -35,7 +39,6 @@ export class SidebarComponent implements OnInit {
       { "itemCode": "204", "itemName": this.language.rule_workbench, "itemNav": "/rule-work-bench", "itemIcon": "#ruleWorkBench", "itemIconSize": "0 0 504.123 504.123", "permission": true },
       { "itemCode": "207", "itemName": this.language.archiving, "itemNav": "/archive", "itemIcon": "#archive", "itemIconSize": "0 0 58 58", "permission": true },
       { "itemCode": "205", "itemName": this.language.config_wizard, "itemNav": "/configuration-wizard", "itemIcon": "#configure", "itemIconSize": "0 0 512 512", "permission": true },
-      { "itemCode": "205", "itemName": this.language.attribute, "itemNav": "/attribute", "itemIcon": "#attributeMaster", "itemIconSize": "0 0 512 512", "permission": true },
       { "itemCode": "208", "itemName": this.language.need_assessment, "itemNav": "/need-assessment", "itemIcon": "#assessmentScreen", "itemIconSize": "0 0 512 512", "permission": true },
       { "itemCode": "209", "itemName": this.language.need_assessment_template, "itemNav": "/need-assessment-template", "itemIcon": "#assessmentTemplate", "itemIconSize": "0 0 512 512", "permission": true },
       { "itemCode": "210", "itemName": this.language.need_assessment_rule, "itemNav": "/need-assessment-rule", "itemIcon": "#assessmentRule", "itemIconSize": "0 0 512 512", "permission": true },
@@ -86,6 +89,16 @@ export class SidebarComponent implements OnInit {
     if (window.innerWidth <= 991) {
       document.getElementById("sidebar-wrapper").classList.remove("toggle");
     }
+  }
+
+  public changNeedToggle() {
+    if (this.needIsToggled) {
+      this.needIsToggled = false;
+    }
+    else {
+      this.needIsToggled = true;
+    }
+
   }
 
 }
